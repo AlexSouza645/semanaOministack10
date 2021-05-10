4491fbc95951722f21da9faa90313d74b632486a
AlexSouza645 <59890212+AlexSouza645@users.noreply.github.com>
2021-05-10 10:45:52 -0300

    devs create

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,25 +1,27 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');// biblioteca que interliga ao mongodb
+const routes = require('./routes')
 const app = express();
 
 
-mongoose.connect('mongodb+srv://oministack:Inquisiti0n6@cluster0.of40q.mongodb.net/oministack10?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0.of40q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
    useNewUrlParser: true,
    useUnifiedTopology:true,
 });
-app.use(express.json());
+app.use(express.json());//faz com q o express entenda o JSON
+app.use(routes);
 
-
+//app.get('/',(request, response)=>{
+// return response.send('hello world')
+//})
 
 //Métodos http: get= buscar informação , post = criar, put= editar usuario, delete
 //tipos de parametros 
 //query params:req.query(filtros, ordenação , paginação)
-
-//route params:equest.params (identificar um recurso na alteração ou remoção ) put, delete
+//route params:request.params (identificar um recurso na alteração ou remoção ) put, delete
 //route params app.delete('/users/:id', (request, response)console.log(request.params)
-
 //body: request.body (dados para criação pou alteração de um registro)
 
-app.listen(3333);    
- 
+app.listen(3333);         
+  
  
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,29 @@
-const {router} = require('express')
-const routes = router();
-routes.post('/users/:id', (request, response) => {
-    console.log(request.body)
-    return response.json({message:' hello world'})
+
+//importar o módulo de roteamento do express com {}
+const { Router } = require('express');
+const axios = require('axios');
+const Dev = require('./models/Dev')
+const routes = Router();
+routes.post('/devs', async (request, response) => {
+    const { github_username, techs } = request.body;
+
+    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
+    const { name = login, avatar_url, bio } = apiResponse.data
+    //if (!name){
+    //  name= apiResponse.data.login;
+    // console.log(name,avatar_url, bioo )
+
+    const techsArray = techs.split(',').map(tech => tech.trim())//trim remove espaçamentos
+
+const dev = await Dev.create({
+    github_username,
+    name,
+    avatar_url,
+    bio,
+    techs: techsArray,
+})
+
+    return response.json(dev);
 });
+//exportar rotas
+module.exports = routes
